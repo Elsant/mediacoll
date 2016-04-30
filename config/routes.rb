764b05b8ed4a8d7102fd6e90d14fae1879f5f666
Elsant <elsant@nextmail.ru>
2016-04-30 22:16:08 +0300

    Adds TempPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'temp_page/home'
+
   devise_for :users
 
   devise_scope :user do
-    root to: "devise/sessions#new"
+    #root to: "devise/sessions#new"
   end
+
+  root to: 'temp_page#home'
 
 end
